2a1c55612364a861efe42d0534df27c15742adc5
codeabuu <abdulngui718@gmail.com>
2024-07-22 17:01:11 -0400

    ball physics calculations for trajectory and velocity

--- physics.py
+++ physics.py
@@ -1,26 +1,21 @@
 import math
 
-def ballpath(startx, starty, power, ang, time):
-    '''
-    calculates the new position of a ball in 2D space after a given time,
-    given its initial position, launch power (velocity), launch angle, and the time elapsed
-    '''
+
+def ballPath(startx, starty, power, ang, time):
     angle = ang
     velx = math.cos(angle) * power
     vely = math.sin(angle) * power
 
     distX = velx * time
-    distY = (velx * time) + ((-9.8 * (time ** 2)) / 2)
+    distY = (vely * time) + ((-9.8 * (time ** 2)) / 2)
 
     newx = round(distX + startx)
     newy = round(starty - distY)
 
     return (newx, newy)
 
+
 def findPower(power, angle, time):
-    '''
-    calculates the final speed of the ball after a given time, given its initial speed and angle
-    '''
     velx = math.cos(angle) * power
     vely = math.sin(angle) * power
 
@@ -29,10 +24,8 @@ def findPower(power, angle, time):
 
     return vf
 
+
 def findAngle(power, angle):
-    '''
-    recalculates the angle of the velocity vector.
-    '''
     velx = math.cos(angle) * power
     vely = math.sin(angle) * power
 
@@ -40,10 +33,8 @@ def findAngle(power, angle):
 
     return ang
 
+
 def maxTime(power, angle):
-    '''
-    calculates the maximum time of flight for the projectile to reach its highest point 
-    '''
     vely = math.sin(angle) * power
     time = ((power * -1) - (math.sqrt(power**2))) / -9.8
 
